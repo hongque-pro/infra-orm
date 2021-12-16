8fbd5c49914d575ba07fdaf80794d10eb669ca12
endink <sharping@outlook.com>
2021-12-17 00:10:04 +0800

    upgrade log4j to 2.16.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ allprojects {
             includeSource = true
             infraBomVersion = Versions.infraBomVersion
             kotlinVersion = Versions.kotlinVersion
-            useMavenProxy = true
+            useMavenProxy = false
         }
 
         useNexusPublish()
