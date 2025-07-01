8167b5bd8ae3782092a447ecaccd82bc1ea76263
endink <sharping@outlook.com>
2025-07-01 23:35:47 +0800

    fix compile deps

--- exposed-core/build.gradle.kts
+++ exposed-core/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
         exclude(group = "org.jetbrains.kotlin")
     }
     compileOnly("org.graalvm.nativeimage:svm:${Versions.graalvmSvm}")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core")
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf")
